1eaf17595ad5e284168bcd3a1d41ac1c428def3b
Ekaterina-Lavrinenko <lavrinenko.ey@gmail.com>
2021-05-31 15:41:12 +0100

    добавляет debounce и pnotify

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,30 +1,69 @@
 import '../css/common.css';
 import countryCardTpl from '../templates/country-card.hbs';
+import countriesListTpl from '../templates/country-list.hbs';
 import API from './api-service';
 import getRefs from './get-refs';
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import debounce from 'lodash.debounce';
 
 const refs = getRefs();
 
-refs.searchForm.addEventListener('input', onSearch);
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
 
 function onSearch(e) {
     e.preventDefault();
 
-    const form = e.currentTarget;
-    const searchQuery = form.elements.query.value;
+    const form = e.target;
+    const searchQuery = form.value;
+
 
     API.fetchCountries(searchQuery)
-    .then(renderCountryCard)
-        .catch(onFetchError)
-        .finally(() => form.reset());
-    }
+        .then(
+            country => {
+                console.log(country);
+                const markup = countryCardTpl(country);
+                const list = countriesListTpl(country);
+                console.log(markup);
+                console.log(country.lenght);
+            
+                if (country.length > 10) {
+                    clearCardContainer();
+                    pnotify()
+                }
+    
+                if (country.length > 1 && country.length <= 10) {
+                    clearCardContainer();
+                    renderCountryCard(list);
+                }
 
-function renderCountryCard(country) {
-        const markup = countryCardTpl(country);
-        refs.cardContainer.innerHTML = markup;
+                if (country.length == 1) {
+                    clearCardContainer();
+                    renderCountryCard(markup);
+                }
+            })
+        .catch(onFetchError)
+        .finally(() => (form.value = ''));
 }
     
-function onFetchError(error) {
+
+function renderCountryCard(tpl) {
+    refs.cardContainer.insertAdjacentHTML('beforeend', tpl);
+}
+
+function clearCardContainer() {
+    refs.cardContainer.innerHTML = '';
+}
+    
+function onFetchError() {
     alert('Oh! Something went wrong!');
+}
+
+function pnotify() {
+    error ({
+        title: 'Oh No!',
+        text: 'Something terrible happened.'
+    })
 }
